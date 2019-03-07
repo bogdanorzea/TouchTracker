38f06cacceb3e72d404fcfde465cbceddc76ff5b
Bogdan Orzea <bogdan.orzea@gmail.com>
2019-03-06 23:44:56 -0500

    Adds Line and DrawView files

---
+++ TouchTracker/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Bogdan Orzea on 3/6/19.
+//  Copyright © 2019 Bogdan Orzea. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Bogdan Orzea on 3/6/19.
+//  Copyright © 2019 Bogdan Orzea. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
